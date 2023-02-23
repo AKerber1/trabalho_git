9b15ded1f180a28271f653c08e97669c0d2ecd54
Arthur Kerber <arthuroliveira.ss009@academico.ifsul.edu.br>
2023-02-23 08:25:51 -0300

    Adicionado print Olá dev

--- TRABALHO_GIT_2K/src/entities/Main.java
+++ TRABALHO_GIT_2K/src/entities/Main.java
@@ -1,11 +1,14 @@
 package entities;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("Olá Mundo");
+		System.out.println("Ola Dev!");
+		
 	}
 
 }
